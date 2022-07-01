898fab922fd518b1d83c3f158807ee1629669afb
rammfall <15bogdan17@gmail.com>
2022-07-01 10:46:49 +0300

    Check

--- src/index.js
+++ src/index.js
@@ -23,7 +23,66 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(`
+    CREATE TABLE users
+    (
+      id   SERIAL PRIMARY KEY,
+      name VARCHAR(30),
+      date TIMESTAMP DEFAULT current_timestamp
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE categories
+    (
+      id   SERIAL PRIMARY KEY,
+      name VARCHAR(30)
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE authors
+    (
+      id   SERIAL PRIMARY KEY,
+      name VARCHAR(30)
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE books
+    (
+      id   SERIAL PRIMARY KEY,
+      title VARCHAR(30),
+      userid INTEGER NOT NULL,
+      authorid INTEGER NOT NULL,
+      categoryid INTEGER NOT NULL,
+      FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (authorid) REFERENCES authors(id) ON DELETE CASCADE,
+      FOREIGN KEY (categoryid) REFERENCES categories(id) ON DELETE CASCADE
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE descriptions
+    (
+      id SERIAL PRIMARY KEY,
+      description VARCHAR(10000),
+      bookid INTEGER NOT NULL UNIQUE,
+      FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+    );
+  `);
+
+  await client.query(`
+    CREATE TABLE reviews
+    (
+      id SERIAL PRIMARY KEY,
+      message VARCHAR(10000),
+      userid INTEGER NOT NULL,
+      bookid INTEGER NOT NULL UNIQUE,
+      FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+    );
+  `);
 
   client.end();
 };
