71d38c9c7126f8fed4d616894d421de1de2dde3a
rdolgopolsky <rdolgopolsky@gmail.com>
2022-07-06 21:37:27 +0300

    HW

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ export const initConnection = () => {
   const client = new Client({
     user: POSTGRES_USER || 'postgres',
     host: POSTGRES_HOST || 'localhost',
-    database: POSTGRES_DB || 'postgres',
-    password: POSTGRES_PASSWORD || 'postgres',
-    port: POSTGRES_PORT || 5556,
+    database: POSTGRES_DB || 'homework_psql',
+    password: POSTGRES_PASSWORD || 'postgress',
+    port: POSTGRES_PORT || 5432,
   });
 
   return client;
@@ -23,8 +23,60 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
-  // Your code is here...
+  await client.query(`
+      CREATE TABLE users (
+         id serial PRIMARY KEY,
+         name VARCHAR(30) NOT NULL,
+         date TIMESTAMP DEFAULT now()
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE categories (
+         id serial PRIMARY KEY,
+         name VARCHAR(30) NOT NULL
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE authors (
+         id serial PRIMARY KEY,
+         name VARCHAR(30) NOT NULL
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE books (
+         id serial PRIMARY KEY,
+         title VARCHAR(30) NOT NULL,
+         userid INTEGER NOT NULL,
+         authorid INTEGER NOT NULL,
+         categoryid INTEGER NOT NULL,
+         FOREIGN KEY (authorid) REFERENCES authors(id) ON DELETE CASCADE,
+         FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+         FOREIGN KEY (categoryid) REFERENCES categories(id) ON DELETE CASCADE
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE descriptions  (
+         id serial PRIMARY KEY,
+         description VARCHAR(10000) NOT NULL,
+         bookid INTEGER UNIQUE NOT NULL,
+         FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE reviews  (
+         id serial PRIMARY KEY,
+         message VARCHAR(10000) NOT NULL,
+         userid INTEGER NOT NULL,
+         bookid INTEGER NOT NULL,
+         FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+         FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+      );
+  `);
 
   client.end();
 };
@@ -33,7 +85,31 @@ export const createItems = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(`
+      INSERT INTO users (name) VALUES ('Roman');
+   `);
+
+  await client.query(`
+      INSERT INTO categories (name) VALUES ('Dystopia');
+   `);
+
+  await client.query(`
+      INSERT INTO authors (name) VALUES ('George Orwell');
+   `);
+
+  await client.query(`
+      INSERT INTO books (title, userid, authorid, categoryid) VALUES ('1984 ', 1, 1, 1);
+   `);
+
+  await client.query(`
+      INSERT INTO descriptions (description, bookid)
+      VALUES ('"1984" is a book that amazes, shocks, breaks your consciousness, destroys stereotypes. A novel-dystopia, on the pages of which you will not want to find yourself! Surprisingly, the realities of the work, written more than 60 years ago, coincide in detail with what we observe today.', 1);
+   `);
+
+  await client.query(`
+      INSERT INTO reviews (message, userid, bookid)
+      VALUES ('An incredible book. Must be read by everyone. And the last part, where it talks about the meaning of language, its amazing...', 1, 1);
+   `);
 
   client.end();
 };
